2ff29a7016875394a8e3b71f757d107bdd203dcd
Haoran Yu <haoranyu6@gmail.com>
2020-04-03 03:26:06 -0700

    initialize the project

---
+++ src/main/java/dev/hyux/joke/springjokeapp/SpringJokeappApplication.java
@@ -0,0 +1,13 @@
+package dev.hyux.joke.springjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hyux/joke/springjokeapp/SpringJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hyux.joke.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
